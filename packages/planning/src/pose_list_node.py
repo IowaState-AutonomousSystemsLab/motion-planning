8ea49b60983d3a36d44fbe9d927622387598bb13
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-02-28 16:18:08 -0600

    add publisher node for list of poses

--- packages/planning/src/pose_list_node.py
+++ packages/planning/src/pose_list_node.py
@@ -14,11 +14,20 @@ class PoseListNode(DTROS):
     def __init__(self, node_name) -> None:
         super(PoseListNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         self.pose_list = []
+        self.pub = rospy.Publisher('pose_list', String, queue_size=10)
 
     def add_pose(self, pose: Pose):
         self.pose_list.append(pose)
 
+    def run(self):
+        rate = rospy.Rate(0.1)
+        while not rospy.is_shutdown():
+            message = f"Nodes: {self.pose_list}"
+            # rospy.loginfo("pose list: '%s'" % message)
+            self.pub.publish(message)
+
 
 if __name__ == "__main__":
     node = PoseListNode(node_name="pose_list")
+    node.run()
     rospy.spin()
