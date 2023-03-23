19ab35b194816a261cc1f7a342cb193865ead310
ranais <ranaisrivastav@gmail.com>
2023-03-23 15:55:56 -0500

    ADding APril Tag Files. Might need a folder restrucutre in the future

---
+++ packages/perception/src/AprilTags.py
@@ -0,0 +1,7 @@
+TILE_LENGTH_CM = 57.
+
+def construct_tranfrm_csv(id, x, y, position_number, rotation, ):
+    """
+
+    """
+    pass
---
+++ packages/perception/src/Tag.py
@@ -0,0 +1,41 @@
+from enum import Enum
+from Position import Position
+
+TILE_LENGTH_CM = 57
+
+class Tag:
+    def __init__(self, april_tag_id: int, coord: tuple[int, int], position: Position, rotation: int):
+        self.id = april_tag_id
+        self.coord = coord
+        self.tag_x_pos = coord[0]
+        self.tag_y_pos = coord[1]
+        self.position = position
+        self.rotation = rotation
+
+    def get_position(self):
+        """ Returns the position as a (x, y) in cm and theta in degrees
+
+        Calculates it given the
+            april_tag_id,
+            coordinate of the tile the tag is on in x,y in tiles
+            the position of the tag in the tile
+            rotation in degrees with 0 as the front of the room (for Auto Lab at ISU)
+        """
+        x_retval = -1
+        y_retval = -1
+
+        if self.position == Position.top_left:
+            pass
+            # x = self.tag_x_pos - 1
+            # y =
+        elif self.position == Position.top_right:
+            pass
+        elif self.position == Position.bot_right:
+            pass
+        elif self.position == Position.bot_left:
+            pass
+        else:
+            Exception("UNKNOWN POSITION WITHIN TILE")
+
+
+
