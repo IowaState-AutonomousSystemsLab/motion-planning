27de69d98b27e998f916f43ab2c71857594e654a
ranais <ranaisrivastav@gmail.com>
2023-04-04 16:04:16 -0500

    Done with reading and writing to AprilTags cvs files

--- packages/perception/src/Tag.py
+++ packages/perception/src/Tag.py
@@ -1,11 +1,11 @@
 from enum import Enum
 from Position import Position
 
-TILE_LENGTH_CM = 57
-DISTANCE_FROM_TILE_EDGE =
-
+TILE_LENGTH_CM = 57.
+DISTANCE_FROM_TILE_EDGE_CM = 2.
 
 class Tag:
+
     def __init__(self, april_tag_id: int, coord: tuple[int, int], position: Position, rotation: int):
         self.id = april_tag_id
         self.coord = coord
@@ -21,22 +21,27 @@ class Tag:
             april_tag_id,
             coordinate of the tile the tag is on in x,y in tiles
             the position of the tag in the tile
-            rotation in degrees with 0 as the front of the room (for Auto Lab at ISU)
+            rotation in degrees. 0 is the TV at the front of the room (for Auto Lab at ISU)
+
+            # >>> Tag(380, (4, 1), Position.top_right, 135)
+
         """
-        x_retval = -1
-        y_retval = -1
 
         if self.position == Position.top_left:
-            x = self.tag_x_pos - 1 * TILE_LENGTH_CM
-            y = self.tag_x_pos - 1 * TILE_LENGTH_CM
+            x = self.tag_x_pos * TILE_LENGTH_CM + DISTANCE_FROM_TILE_EDGE_CM
+            y = (self.tag_y_pos + 1) * TILE_LENGTH_CM
         elif self.position == Position.top_right:
-            x = self.tag_x_pos - 1 * TILE_LENGTH_CM
-            y = self.tag_x_pos - 1 * TILE_LENGTH_CM
+            x = (self.tag_x_pos + 1) * TILE_LENGTH_CM - 2 * DISTANCE_FROM_TILE_EDGE_CM
+            y = (self.tag_y_pos + 1) * TILE_LENGTH_CM - 2 * DISTANCE_FROM_TILE_EDGE_CM
         elif self.position == Position.bot_right:
-            x = self.tag_x_pos - 1 * TILE_LENGTH_CM
-            y = self.tag_x_pos - 1 * TILE_LENGTH_CM
+            x = (self.tag_x_pos + 1) * TILE_LENGTH_CM - 2 * DISTANCE_FROM_TILE_EDGE_CM
+            y = self.tag_y_pos * TILE_LENGTH_CM
         elif self.position == Position.bot_left:
-            x = self.tag_x_pos - 1 * TILE_LENGTH_CM
-            y = self.tag_x_pos - 1 * TILE_LENGTH_CM
+            x = self.tag_x_pos * TILE_LENGTH_CM
+            y = self.tag_y_pos * TILE_LENGTH_CM
         else:
+            x = None
+            y = None
             Exception("UNKNOWN POSITION WITHIN TILE")
+
+        return self.id, x, y, self.rotation
