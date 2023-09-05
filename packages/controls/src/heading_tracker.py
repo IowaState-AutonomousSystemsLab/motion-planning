e8a84c6d07780ab4fe293bbaecede9453aa61408
Ranai <ranais@iastate.edu>
2023-09-05 16:05:44 -0500

    adding user io

--- packages/controls/src/heading_tracker.py
+++ packages/controls/src/heading_tracker.py
@@ -12,7 +12,7 @@ from include.PIDF import PIDF
 
 from sensor_msgs.msg import Imu
 from as_msgs.msg import WheelOdometry
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Bool
 from geometry_msgs.msg import PoseStamped
 from duckietown_msgs.msg import WheelsCmdStamped
 from offset_calculator import OffsetCalculator
@@ -47,32 +47,49 @@ class HeadingTracker():
         self.curr_pose = -255
         self.pose_sub = rospy.Subscriber("<CHANGE THIS>", PoseStamped, callback = self.update_pos)
 
-        #Wheel speed speed tracking
+        # Wheel speed speed tracking
         self.left_speed  = -255
         self.right_speed = -255
-        self.odometery_sub = rospy.Subscriber(f"~wheel_odometry/odometry",WheelOdometry,self.odometry_cb)
+        self.odometery_sub = rospy.Subscriber(f"~wheel_odometry/odometry", WheelOdometry,self.odometry_cb)
 
-        #Wheel speed publisher
+        # Wheel speed publisher
         self.wheel_pub = rospy.Publisher(f"/{os.environ['VEHICLE_NAME']}/wheels_driver_node/wheels_cmd", WheelsCmdStamped, queue_size=1)
 
-        #Wheel speed PIDs
+        # Wheel speed PIDs
         self.left_pid  = PIDF(self.left_wheel_kP,  self.left_wheel_kI,  self.left_wheel_kD,  0)
         self.right_pid = PIDF(self.right_wheel_kP, self.right_wheel_kI, self.right_wheel_kD, 0)
         self.heading_pid = PIDF(self.heading_kP, self.heading_kI, self.heading_kD, 0)
         
-        #Globals
+        # Globals
         x_ref = -255
         y_ref = -255
 
+        # User IO
+        self.enable
+        self.enable_pub = rospy.Publisher(f"/{os.environ['VEHICLE_NAME']}/supervisor/experiment_enable", Bool)
+        self.enable_sub = rospy.Subscriber(f"/{os.environ['VEHICLE_NAME']}/supervisor/experiment_enable", Bool, self.enable_handler)
+
         # rospy.loginfo("     CALCULATING IMU OFFSET")
         # offfset_calc = OffsetCalculator()
+
+        # Make the robot stop cleanly
+        rospy.on_shutdown(self.zero_command)
 
         rospy.loginfo("initialized")
 
     def truncate(self, num:float, dec_places:int):
         mult = 10**dec_places
         return int(num * ()) * mult / mult
+    
+    def on_goal_reached(self, data: Bool):
+        self.enable = False
+        self.zero_command()
 
+    def enable_handler(self, data: Bool):
+        rate = rospy.Rate(10)
+        while self.enable:
+            self.track_heading_and_speed()
+            rate.sleep()
 
     def integrate(self, imu_out:Imu):
         """
@@ -81,7 +98,7 @@ class HeadingTracker():
         self.heading = self.heading + (self.truncate(imu_out.angular_velocity.x, 4) - 0.005)
         rospy.logdebug(" + Heading Values = " + str(self.heading))
 
-    def update_pos(seld, data):
+    def update_pos(self, data):
         pass
 
     def track_heading_and_speed(self, des_heading, curr_heading, des_speed, curr_speed):
@@ -116,8 +133,8 @@ class HeadingTracker():
         self.y_ref = y_ref
         
     def track_point(self):
+        # TODO add a conditional that ssays when Goal is reached, call a different method
         pass
-        
 
 
     def set_wheel_speeds(self, left_speed:float, right_speed:float):
@@ -132,16 +149,9 @@ class HeadingTracker():
         wheelsCmd.vel_right = self.right_pid.update(right_speed,0.1)
         self.wheel_pub.publish(wheelsCmd)
 
-    def run(self):
-        rate = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            self.track_heading_and_speed()
-            rate.sleep()
 
-
-    def on_shutdown(self):
+    def zero_command(self):
     #send a zero velocity wheel command
-
         wheelsCmd = WheelsCmdStamped()
         header = Header()
         wheelsCmd.header = header
